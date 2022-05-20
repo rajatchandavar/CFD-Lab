17b5551f1f0fd4c974bda6470c2f99f59d3015e7
ge47gem <hsumukha@tum.de>
2022-05-20 12:34:04 +0200

    BC completed

--- src/Boundary.cpp
+++ src/Boundary.cpp
@@ -23,6 +23,13 @@ void FixedWallBoundary::apply(Fields &field) {
             field.p(i, j) = field.p(i, j + 1);
         }
 
+        // Top Wall
+        if(currentCell->is_border(border_position::BOTTOM)){
+            field.u(i, j) = -field.u(i, j - 1);
+            field.v(i, j) = 0.0;
+            field.p(i, j) = field.p(i, j - 1);
+        }
+
         // Left Wall
         if(currentCell->is_border(border_position::RIGHT)){
             field.u(i, j) = 0.0;
@@ -65,21 +72,40 @@ void MovingWallBoundary::apply(Fields &field) {
     }
 }
 
+InFlowBoundary::InFlowBoundary(std::vector<Cell *> cells, double UIN, double VIN) : _cells(cells) {
+    _UIN.insert(std::pair(GEOMETRY_PGM::inflow_id, UIN));
+    _VIN.insert(std::pair(GEOMETRY_PGM::inflow_id, VIN));
+}
+
+InFlowBoundary::InFlowBoundary(std::vector<Cell *> cells, std::map<int, double> UIN, std::map<int, double> VIN,
+                                       std::map<int, double> wall_temperature)
+    : _cells(cells), _UIN(UIN), _VIN(VIN), _wall_temperature(wall_temperature) {}
+
 void InFlowBoundary::apply(Fields &field) {
     for (auto currentCell: _cells){
         int i = currentCell->i();
         int j = currentCell->j();
             field.u(i,j) = _UIN[1];
-            field.v(i,j) = _VIN[1];
+            field.v(i,j) = 2 * _VIN[1] - field.v(i + 1, j);
             field.p(i,j) = field.p(i + 1, j);
     }
 }
+
+OutFlowBoundary::OutFlowBoundary(std::vector<Cell *> cells, double POUT) : _cells(cells) {
+    _POUT.insert(std::pair(GEOMETRY_PGM::outflow_id, POUT));
+}
+
+OutFlowBoundary::OutFlowBoundary(std::vector<Cell *> cells, std::map<int, double> POUT,
+                                       std::map<int, double> wall_temperature)
+    : _cells(cells), _POUT(POUT), _wall_temperature(wall_temperature) {}
 
 void OutFlowBoundary::apply(Fields &field) {
     for (auto currentCell: _cells){
         int i = currentCell->i();
         int j = currentCell->j();
 
-            field.p(i,j) = _POUT;
+            field.u(i,j) = field.u(i - 1,j);
+            field.v(i,j) = field.v(i - 1,j);
+            field.p(i,j) = 2 * _POUT[2] - field.p(i - 1, j);
     }
 }
