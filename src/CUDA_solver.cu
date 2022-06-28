a06d1ba930d52975bf2c70abfa734dc90786ceaa
hsumukha <hsumukha@tum.de>
2022-06-28 20:32:20 +0000

    minoe debugging

--- src/CUDA_solver.cu
+++ src/CUDA_solver.cu
@@ -67,7 +67,9 @@ dim3 CUDA_solver::get_num_blocks_2d(int size_x, int size_y){
 
 //Boundaries
 
-__device__ void FixedWallBoundary(double *U, double *V, double *P, double *T, int i, int j) {
+__global__ void FixedWallBoundary(double *U, double *V, double *P, double *T, int i, int j, int *geometry_data,
+int *fluid_id, int *moving_wall_id, int *fixed_wall_id, int *inflow_id, int *outflow_id, int *adiabatic_id, int *hot_id,
+int *cold_id, double *wall_temp_a, double *wall_temp_h, double *wall_temp_c, bool isHeatTransfer) {
 
         // obstacles B_NE (Corner Cell) - This section applies the appropriate boundary conditions to a fixed wall with fluid cells on 
         // the North and East directions 
@@ -83,9 +85,9 @@ __device__ void FixedWallBoundary(double *U, double *V, double *P, double *T, in
                 if(at(geometry_data,i,j) == adiabatic_id)
                     at(T, i, j) = (at(T, i + 1, j) + at(T, i, j + 1))/2;
                 else if (at(geometry_data,i,j) == hot_id)
-                    at(T,i, j) = 2*wall_temp_h - (at(T,i, j + 1) + at(T,i + 1, j) )/2;
+                    at(T,i, j) = 2*(*wall_temp_h) - (at(T,i, j + 1) + at(T,i + 1, j) )/2;
                 else if (at(geometry_data,i,j) == cold_id)
-                    at(T,i, j) = 2*wall_temp_c - (at(T,i, j + 1) + at(T,i + 1, j) )/2;
+                    at(T,i, j) = 2*(*wall_temp_c) - (at(T,i, j + 1) + at(T,i + 1, j) )/2;
                 }
 
     }
@@ -105,9 +107,9 @@ __device__ void FixedWallBoundary(double *U, double *V, double *P, double *T, in
                     if(at(geometry_data,i,j) == adiabatic_id)
                     at(T,i, j) = (at(T, i + 1, j) + at(T, i, j - 1))/2;
                     else if (at(geometry_data,i,j) == hot_id)
-                        at(T, i, j) = 2*wall_temp_h - (at(T,i, j - 1) + at(T,i + 1, j) )/2;
+                        at(T, i, j) = 2*(*wall_temp_h) - (at(T,i, j - 1) + at(T,i + 1, j) )/2;
                     else if (at(geometry_data,i,j) == cold_id)
-                        at(T,i, j) = 2*wall_temp_c - (at(T,i, j - 1) + at(T,i + 1, j) )/2;
+                        at(T,i, j) = 2*(*wall_temp_c) - (at(T,i, j - 1) + at(T,i + 1, j) )/2;
                 }
 
             }
@@ -127,9 +129,9 @@ __device__ void FixedWallBoundary(double *U, double *V, double *P, double *T, in
                     if(at(geometry_data,i,j) == adiabatic_id)
                         at(T,i, j) = (at(T,i - 1, j) + at(T,i, j + 1))/2;
                     else if (at(geometry_data,i,j) == hot_id)
-                        at(T,i, j) = 2*wall_temp_h - (at(T,i, j + 1) + at(T,i - 1, j) )/2;
+                        at(T,i, j) = 2*(*wall_temp_h) - (at(T,i, j + 1) + at(T,i - 1, j) )/2;
                     else if (at(geometry_data,i,j) == cold_id)
-                        at(T,i, j) = 2*wall_temp_c - (at(T,i, j + 1) + at(T,i - 1, j) )/2;
+                        at(T,i, j) = 2*(*wall_temp_c) - (at(T,i, j + 1) + at(T,i - 1, j) )/2;
                 }
 
         }
@@ -148,9 +150,9 @@ __device__ void FixedWallBoundary(double *U, double *V, double *P, double *T, in
                 if(at(geometry_data,i,j) == adiabatic_id)
                    at(T,i, j) = (at(T,i - 1, j) + at(T,i, j - 1))/2;
                 else if (at(geometry_data,i,j) == hot_id)
-                    at(T,i, j) = 2*wall_temp_h - (at(T,i, j - 1) + at(T,i - 1, j) )/2;
+                    at(T,i, j) = 2*(*wall_temp_h) - (at(T,i, j - 1) + at(T,i - 1, j) )/2;
                 else if (at(geometry_data,i,j) == cold_id)
-                    at(T,i, j) = 2*wall_temp_c - (at(T,i, j - 1) + at(T,i - 1, j) )/2;
+                    at(T,i, j) = 2*(*wall_temp_c) - (at(T,i, j - 1) + at(T,i - 1, j) )/2;
             }
             
         }
@@ -167,9 +169,9 @@ __device__ void FixedWallBoundary(double *U, double *V, double *P, double *T, in
                 if(at(geometry_data,i,j) == adiabatic_id)
                     at(T,i, j) = at(T,i, j + 1);
                 else if (at(geometry_data,i,j) == hot_id)
-                    at(T,i, j) = 2*wall_temp_h - at(T,i, j + 1);
+                    at(T,i, j) = 2*(*wall_temp_h) - at(T,i, j + 1);
                 else if (at(geometry_data,i,j) == cold_id)
-                    at(T,i, j) = 2*wall_temp_c - at(T,i, j + 1);
+                    at(T,i, j) = 2*(*wall_temp_c) - at(T,i, j + 1);
             }
         }
 
@@ -186,9 +188,9 @@ __device__ void FixedWallBoundary(double *U, double *V, double *P, double *T, in
                 if(at(geometry_data,i,j) == adiabatic_id)
                     at(T,i, j) = at(T,i, j - 1);
                 else if (at(geometry_data,i,j) == hot_id)
-                    at(T,i, j) = 2 * wall_temp_h - at(T,i, j - 1);
+                    at(T,i, j) = 2 * (*wall_temp_h) - at(T,i, j - 1);
                 else if (at(geometry_data,i,j) == cold_id)
-                    at(T,i, j) = 2 * wall_temp_c - at(T,i, j - 1);
+                    at(T,i, j) = 2 * (*wall_temp_c) - at(T,i, j - 1);
             }
         }
 
@@ -204,9 +206,9 @@ __device__ void FixedWallBoundary(double *U, double *V, double *P, double *T, in
                 if(at(geometry_data,i,j) == adiabatic_id)
                     at(T,i, j) = at(T,i + 1, j);
                 else if (at(geometry_data,i,j) == hot_id) 
-                    at(T,i, j) = 2*wall_temp_h - at(T,i + 1, j);        
+                    at(T,i, j) = 2*(*wall_temp_h) - at(T,i + 1, j);        
                 else if (at(geometry_data,i,j) == cold_id)
-                    at(T,i, j) = 2*wall_temp_c - at(T,i + 1, j);
+                    at(T,i, j) = 2*(*wall_temp_c) - at(T,i + 1, j);
             }
         }
 
@@ -225,28 +227,28 @@ __device__ void FixedWallBoundary(double *U, double *V, double *P, double *T, in
                 if(at(geometry_data,i,j) == adiabatic_id)
                     at(T,i, j) = at(T,i - 1, j);
                 else if (at(geometry_data,i,j) == hot_id)
-                    at(T,i, j) = 2*wall_temp_h - at(T,i - 1, j);
+                    at(T,i, j) = 2*(*wall_temp_h) - at(T,i - 1, j);
                 else if (at(geometry_data,i,j) == cold_id)
-                    at(T,i, j) = 2*wall_temp_c - at(T,i - 1, j);
+                    at(T,i, j) = 2*(*wall_temp_c) - at(T,i - 1, j);
             }
         }
 }
 
-__device__ void MovingWallBoundary(double *U, double *V, double *P, double *T, int i, int j) {
+__global__ void MovingWallBoundary(double *U, double *V, double *P, double *T, int i, int j, double *wall_velocity) {
         
-        at(U,i, j) = 2 * (wall_velocity- at(U,i, j-1));
+        at(U,i, j) = 2 * wall_velocity- at(U,i, j-1);
         //Since v grid is staggered, the v velocity of cells to below of ghost layer should be set to 0.
         at(V,i,j - 1) = 0.0;
         at(P,i,j) = at(P,i, j-1);
 }
 
-__device__ void InFlowBoundary(double *U, double *V, double *P, double *T, int i, int j) {
+__global__ void InFlowBoundary(double *U, double *V, double *P, double *T, int i, int j, double *UIN, double *VIN) {
         at(U,i,j) = UIN;
         at(V,i,j) = 2 * VIN - at(V,i + 1, j);
         at(P,i,j) = at(P,i + 1, j);
 }
 
-__device__ void OutFlowBoundary(double *U, double *V, double *P, double *T, int i, int j) {
+__global__ void OutFlowBoundary(double *U, double *V, double *P, double *T, int i, int j, double *POUT) {
 
             at(U,i,j) = at(U,i - 1,j);
             at(V,i,j) = at(V,i - 1,j);
@@ -258,7 +260,8 @@ __global__ void calc_T_kernel(double * T, double *T_temp, double *U, double *V, 
     int j = blockIdx.y * blockDim.y + threadIdx.y;
     if (i < *size_x && j < *size_y)
         at(T,i,j) = (*dt) * (*alpha * diffusion(T_temp,i,j, *dx, *dy, size_x) - convection_Tu(T_temp,U,i,j,*dx, *dy, *gamma, size_x) - convection_Tv(T_temp,V,i,j, *dx, *dy, *gamma, size_x)) + at(T_temp, i,j);
-}   
+}  
+
 CUDA_solver::CUDA_solver(Fields &field, Grid &grid){
 
     block_size = dim3(BLOCK_SIZE);
@@ -332,26 +335,38 @@ void CUDA_solver::pre_process(Fields &field, Grid &grid, Discretization &discret
     cudaMemcpy(size_x, &grid_size_x, sizeof(int),cudaMemcpyHostToDevice);
     cudaMemcpy(size_y, &grid_size_y, sizeof(int),cudaMemcpyHostToDevice);
 
-    cudaMemcpy(moving_wall_id, &(GEOMETRY_PGM::moving_wall_id), sizeof(int),cudaMemcpyHostToDevice);
-    cudaMemcpy(fixed_wall_id, &(GEOMETRY_PGM::fixed_wall_id), sizeof(int),cudaMemcpyHostToDevice);
-    cudaMemcpy(inflow_id, &(GEOMETRY_PGM::inflow_id), sizeof(int),cudaMemcpyHostToDevice);
-    cudaMemcpy(outflow_id, &(GEOMETRY_PGM::outflow_id), sizeof(int),cudaMemcpyHostToDevice);
-    cudaMemcpy(hot_id, &(GEOMETRY_PGM::hot_id), sizeof(int),cudaMemcpyHostToDevice);
-    cudaMemcpy(cold_id, &(GEOMETRY_PGM::cold_id), sizeof(int),cudaMemcpyHostToDevice);
-    cudaMemcpy(adiabatic_id, &(GEOMETRY_PGM::adiabatic_id), sizeof(int),cudaMemcpyHostToDevice);
+    int var = GEOMETRY_PGM::moving_wall_id;
+    cudaMemcpy(moving_wall_id, &var, sizeof(int),cudaMemcpyHostToDevice);
+    var = GEOMETRY_PGM::fixed_wall_id;
+    cudaMemcpy(fixed_wall_id, &var, sizeof(int),cudaMemcpyHostToDevice);
+    var = GEOMETRY_PGM::inflow_id;
+    cudaMemcpy(inflow_id, &var, sizeof(int),cudaMemcpyHostToDevice);
+    var = GEOMETRY_PGM::outflow_id;
+    cudaMemcpy(outflow_id, &var, sizeof(int),cudaMemcpyHostToDevice);
+    var = GEOMETRY_PGM::hot_id;
+    cudaMemcpy(hot_id, &var, sizeof(int),cudaMemcpyHostToDevice);
+    var = GEOMETRY_PGM::cold_id;
+    cudaMemcpy(cold_id, &var, sizeof(int),cudaMemcpyHostToDevice);
+    var = GEOMETRY_PGM::adiabatic_id;
+    cudaMemcpy(adiabatic_id, &var, sizeof(int),cudaMemcpyHostToDevice);
 
-    cudaMemcpy(wall_temp_a, &wall_temp_a, sizeof(double),cudaMemcpyHostToDevice);
-    cudaMemcpy(wall_temp_h, &wall_temp_h, sizeof(double),cudaMemcpyHostToDevice);
-    cudaMemcpy(wall_temp_c, &wall_temp_c, sizeof(double),cudaMemcpyHostToDevice);
-    cudaMemcpy(isHeatTransfer, &field.isHeatTransfer(), sizeof(bool),cudaMemcpyHostToDevice);
+    double var = wall_temp_a;
+    cudaMemcpy(wall_temp_a, &var, sizeof(double),cudaMemcpyHostToDevice);
+    var = wall_temp_h;
+    cudaMemcpy(wall_temp_h, &var, sizeof(double),cudaMemcpyHostToDevice);
+    var = wall_temp_c;
+    cudaMemcpy(wall_temp_c, &var, sizeof(double),cudaMemcpyHostToDevice);
+    int var = field.isHeatTransfer();
+    cudaMemcpy(isHeatTransfer, &var, sizeof(int),cudaMemcpyHostToDevice);
 
-    cudaMemcpy(wall_velocity, &(LidDrivenCavity::wall_velocity), sizeof(double),cudaMemcpyHostToDevice);
-    cudaMemcpy(UIN, &UIN, sizeof(double),cudaMemcpyHostToDevice);
-    cudaMemcpy(VIN, &VIN, sizeof(double),cudaMemcpyHostToDevice);
-    cudaMemcpy(POUT, &(GEOMETRY_PGM::POUT), sizeof(double),cudaMemcpyHostToDevice);
-
-
-
+    double var = LidDrivenCavity::wall_velocity;
+    cudaMemcpy(wall_velocity, &var, sizeof(double),cudaMemcpyHostToDevice);
+    var = UIN;
+    cudaMemcpy(UIN, &var, sizeof(double),cudaMemcpyHostToDevice);
+    var = VIN;
+    cudaMemcpy(VIN, &var, sizeof(double),cudaMemcpyHostToDevice);
+    var = GEOMETRY_PGM::POUT;
+    cudaMemcpy(POUT, &var, sizeof(double),cudaMemcpyHostToDevice);
 }
 
 void CUDA_solver::calc_T(){
@@ -359,8 +374,7 @@ void CUDA_solver::calc_T(){
     num_blocks_2d = get_num_blocks_2d(grid_size_x, grid_size_y);
     calc_T_kernel<<<num_blocks_2d, block_size_2d>>>(T, T_temp, U, V, dx, dy, dt, alpha, gamma, size_x, size_y);
 }
-
-__global__ apply_boundary() {
+void apply_boundary() {
 
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     int j = blockIdx.y * blockDim.y + threadIdx.y;
@@ -369,16 +383,16 @@ __global__ apply_boundary() {
     if (i < *size_x && j < *size_y) {
 
         if (at(geometry_data,i,j)==3 || at(geometry_data,i,j)==5 || at(geometry_data,i,j)== 6 || at(geometry_data,i,j)== 7)
-            FixedWallBoundary(U, V, P, T, i, j);
+            FixedWallBoundary<<<num_blocks_2d, block_size_2d>>>(U, V, P, T, i, j, geometry_data, fluid_id, moving_wall_id, fixed_wall_id, inflow_id, outflow_id, adiabatic_id, hot_id, cold_id, wall_temp_a, wall_temp_h, wall_temp_c, isHeatTransfer);
 
         else if (at(geometry_data,i,j)==8)
-            MovingWallBoundary(U, V, P, T, i, j);
+            MovingWallBoundary<<<num_blocks_2d, block_size_2d>>>(U, V, P, T, i, j, wall_velocity);
 
         else if (at(geometry_data,i,j)==1)
-            InFlowBoundary(U, V, P, T, i, j);
+            InFlowBoundary<<<num_blocks_2d, block_size_2d>>>(U, V, P, T, i, j, UIN, VIN);
 
         else (at(geometry_data,i,j)==2)
-            OutFlowBoundary(U, V, P, T, i, j);
+            OutFlowBoundary<<<num_blocks_2d, block_size_2d>>>(U, V, P, T, i, j, POUT);
 
     }
 
@@ -393,8 +407,8 @@ __device__ void calc_fluxes(double *F, double *G, double *U, double *V, double g
     
     if(isHeatTransfer){
        
-            at(F,i,j) = at(F,i,j) - beta * dt / 2.0 * (at(T,(i,j) + at(T,(i + 1, j)) * gx - dt * gx;
-            at(G,i,j) = at(G,i,j) - beta * dt / 2.0 * (at(T,(i,j) + at(T,(i, j + 1)) * gy - dt * gy;
+            at(F,i,j) = at(F,i,j) - beta * dt / 2.0 * (at(T,i,j) + at(T,i + 1, j)) * gx - dt * gx;
+            at(G,i,j) = at(G,i,j) - beta * dt / 2.0 * (at(T,i,j) + at(T,i, j + 1)) * gy - dt * gy;
     
     }
     
